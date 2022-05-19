7592fe52954231cf6df72d74bf498daba1c1599c
Daven Young <davenyoung@msn.com>
2022-05-19 10:55:46 -0600

    commit tree 3eb019deee2647b068b3e5abf7776fe8e8bc0336

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
-
+    attr_accessor :name
+    attr_accessor :breed
+    def name(name="Fido")
+        @name = name
+    end
+    def breed(breed="Beagle")
+        @breed = breed
+    end
 end
+
+def name
+    @name
+end
+def breed
+    @breed
+end
+
+fido = Dog.new
+fido.name=('Fido')
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
-
+    attr_accessor :name
+    attr_accessor :job
+    def name(name="Beyonce")
+        @name = name
+    end
+    def breed(job="Singer")
+        @job = job
+    end
 end
+
+def name
+    @name
+end
+def breed
+    @job
+end
+
+beyonce = Person.new
+beyonce.name=('Beyonce')
